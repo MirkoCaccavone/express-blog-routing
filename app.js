62c8c5eecc1e95b4296a1d2ec1190165c0361f56
Mirko <mirkocaccavone03@gmail.com>
2025-02-05 14:35:19 +0100

    create folders and file, inizialize express

---
+++ app.js
@@ -0,0 +1,17 @@
+// Importa il modulo Express
+const express = require('express')
+const app = express()
+const port = 3000
+const postsRouter = require('./routers/posts');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send("Server del mio blog")
+});
+
+app.use("/posts", postsRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
